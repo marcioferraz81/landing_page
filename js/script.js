5e697469f52086495ecb917913e5991b3736b171
marcioferraz81 <marcioferraz81@gmail.com>
2024-10-07 22:07:34 -0300

    landing page

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling effect for anchors
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
